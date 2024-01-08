e3b93f765d44022da67e272f788f6224c74a6023
Joseph McLaughlin <josephmclaughlin@me.com>
2024-01-07 18:14:34 -0700

    update to use new fish abbrs

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,11 @@
 starship init fish | source
+
+# Abbreviations
+abbr -a -- gs 'git status'
+abbr -a -- blog 'hugo server -D -p 1313'
+abbr -a -- xyz 'hugo server -D -p 1314'
+abbr -a -- gl 'git log --oneline -20'
+abbr -a -- gsu 'git submodule update --init --recursive'
+abbr -a -- gd 'git checkout develop && git pull'
+abbr -a -- code 'cd ~/Developer'
+abbr -a -- dev 'cd ~/Developer'
